88e2d7502f87515923dbe14d4901fffe43d0a490
Camirand <camirand.mac@gmail.com>
2023-09-01 18:34:13 -0400

    removed layout

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,16 @@
-import RootLayout from "../layouts/RootLayout";
+import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import "../styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className={`flex flex-col min-h-screen ${inter.className}`}>
       <Navbar />
       <div className="flex-grow">
-        <RootLayout>
-          <Component {...pageProps} />
-        </RootLayout>
+        <Component {...pageProps} />
       </div>
       <Footer />
     </div>
