4cb8390cae45c6b9be5f90a2970e87f095cc83ca
Camirand <camirand.mac@gmail.com>
2023-09-03 22:23:37 -0400

    feat: Added line under active and animated x

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,16 +21,15 @@ const NavLink = ({ href, text }) => {
 
   return (
     <Link href={href}>
-      <motion.div className="relative">
-        {isActive && (
-          <motion.div
-            initial={{ scaleX: 0 }}
-            animate={{ scaleX: 1 }}
-            transition={{ duration: 0.9, ease: "easeInOut" }}
-            className="absolute bottom-[-2px] left-0 h-[2px] bg-blue origin-left"
-            style={{ width: textWidth }}
-          />
-        )}
+      <motion.div className="relative group">
+        <motion.div
+          initial={{ scaleX: 0 }}
+          animate={{ scaleX: isActive ? 1 : 0 }}
+          whileHover={{ scaleX: 1 }}
+          transition={{ duration: 0.3, ease: "easeInOut" }}
+          className="absolute h-[2px] bg-blue bottom-[-1px] left-0 origin-left"
+          style={{ width: textWidth }}
+        />
         <span
           ref={textRef}
           className={`font-bold pb-1 ${
@@ -67,8 +66,24 @@ const Navbar = () => {
         className="lg:hidden block text-secondary z-20"
         onClick={() => setIsOpen(!isOpen)}
       >
-        {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+        {isOpen ? (
+          <motion.div
+            initial={{ rotate: 0 }}
+            animate={{ rotate: 270 }}
+            transition={{
+              type: "spring",
+              stiffness: 10,
+              damping: 1,
+              duration: 1,
+            }}
+          >
+            <FaTimes size={24} />
+          </motion.div>
+        ) : (
+          <FaBars size={24} />
+        )}
       </button>
+
       <AnimatePresence>
         {isOpen && (
           <motion.div
@@ -77,7 +92,7 @@ const Navbar = () => {
             exit={{ opacity: 0 }}
             className="fixed top-0 left-0 w-full h-full bg-primary flex flex-col justify-center items-center z-10 lg:hidden"
           >
-            <div onClick={closeMenu} className="mb-6 space-y-4">
+            <div onClick={closeMenu} className="mb-6 space-y-8">
               <NavLink href="/" text="Accueil" />
               <NavLink href="/realisations" text="Réalisations" />
               <NavLink href="/experiences" text="Expériences" />
@@ -85,13 +100,14 @@ const Navbar = () => {
             <Button link="/contact" text="Embauchez-moi" onClick={closeMenu} />
           </motion.div>
         )}
+
+        <div className="hidden lg:flex flex-row items-center space-x-6">
+          <NavLink href="/" text="Accueil" />
+          <NavLink href="/realisations" text="Réalisations" />
+          <NavLink href="/experiences" text="Expériences" />
+          <Button link="/contact" text="Embauchez-moi" />
+        </div>
       </AnimatePresence>
-      <div className="hidden lg:flex flex-row items-center space-x-6">
-        <NavLink href="/" text="Accueil" />
-        <NavLink href="/realisations" text="Réalisations" />
-        <NavLink href="/experiences" text="Expériences" />
-        <Button link="/contact" text="Embauchez-moi" />
-      </div>
     </motion.nav>
   );
 };
