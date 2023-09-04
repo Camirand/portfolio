174292affa873bb3e808f120d878f655645b5390
Camirand <camirand.mac@gmail.com>
2023-09-04 07:55:40 -0400

    Feat: Added animation to light button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Button.css";
 
 export const Button = ({ text, link, target, onClick }) => {
   return (
@@ -14,12 +15,15 @@ export const Button = ({ text, link, target, onClick }) => {
 
 export const ButtonLight = ({ text, link, target, onClick }) => {
   return (
-    <div onClick={onClick}>
-      <a href={link} target={target} rel="noopener noreferrer">
-        <button
-          className="bg-primary text-secondary font-bold px-4 py-2 rounded-full hover:bg-secondary hover:text-primary transition-all"
-          target={target}
-        >
+    <div onClick={onClick} className="relative hover-fill-effect">
+      <div className="button-background"></div>
+      <a
+        href={link}
+        target={target}
+        rel="noopener noreferrer"
+        className="inline-block"
+      >
+        <button className="button-text" target={target}>
           {text}
         </button>
       </a>
